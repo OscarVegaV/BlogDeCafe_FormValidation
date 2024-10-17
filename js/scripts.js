d9de35427141bb5bb0bde66752c9d4a1ccb744ab
Oscar Vega <os_ar1@hotmail.com>
2024-10-17 09:47:54 -0600

    Created new HTML elements using createElement and added them to the DOM.

--- js/scripts.js
+++ js/scripts.js
@@ -15,13 +15,35 @@ console.log(links);// Logs a NodeList of all <a> elements found
 console.log(links[0]); // Logs the first <a> element found
 links[0].textContent = 'Nuevo Texto para Enlace';// Change the text content of the first link
 links[0].classList.add('new-class');// Adds a new class to the first link
-links[0].classList.remove('link__navegation'); // Removes the old class from the first link
-
-
-
+// links[0].classList.remove('link__navegation'); // Removes the old class from the first link
 
 //  ** querySelectorById **
 // Selects the element with the ID 'heading' (considered an older method)
 
-const heading2 = document.getElementById('heading');
-console.log(heading2);// Logs the element with the ID 'heading'
+/*const heading2 = document.getElementById('heading');
+console.log(heading2);// Logs the element with the ID 'heading'*/
+
+//Generating a new link 
+
+// this code makes an HTML link element
+const newLink = document.createElement('A');
+
+//adding href
+newLink.href ='new-link.html';
+
+// adding text
+newLink.textContent = 'Un Nuevo Enlace';
+
+//adding class
+newLink.classList.add = 'link__navegation';
+//newLink.classList.add('link__navegation'); // Corrected line
+
+// adding from Document
+const navegation = document.querySelector('.navegation');
+
+// Adding the new link to the navigation
+// The appendChild() method is used to add a new child node (newLink) 
+// as the last child of the specified parent node (navegation).
+navegation.appendChild(newLink);
+
+console.log(newLink);
